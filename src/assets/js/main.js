f267b0e9ea9b7b4ca31ab746c6fe5753fd4de4e7
Kamil Chyrek <kamil.chyrek@chilid.pl>
2016-10-08 19:47:00 +0200

    final

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -41,7 +41,7 @@ $(".workers-list__button").on("click", function() {
                     data: { get_param: 'value' },
                     dataType: 'json',
                     success: function (data) {
-                        var container = "<div class=profile-full data-id=" + data.Id + "><img src=assets/images/photo_" + data.Id + ".png alt class=photo><table><tr><td>IMIĘ</td><td>" + data.Name + "</td></tr><tr><td>NAZWISKO</td><td>" + data.Surname + "</td></tr><tr><td>STANOWISKO</td><td data-id=" + data.RoleId + ">" + data.Role +  "</td></tr><tr><td>TECHNOLOGIE</td><td>" + data.Technologies +  "</td></tr><tr><td>ZAINTERESOWANIA</td><td>" + data.Hobbies + "</table></div>";
+                        var container = "<div class=profile-full data-id=" + data.Id + "><img src=assets/images/photo_" + data.Id + ".png alt class=photo><table><tr><td>IMIĘ</td><td>" + data.Name + "</td></tr><tr><td>NAZWISKO</td><td>" + data.Surname + "</td></tr><tr><td>STANOWISKO</td><td data-id=" + data.RoleId + ">" + data.Role +  "</td></tr><tr><td>TECHNOLOGIE</td><td>" + data.Technologies +  "</td></tr><tr><td>ZAINTERESOWANIA</td><td>" + data.Hobbies + "</td></tr></table></div>";
                         $('#workers__container').append(container);
                     },
                     error: function() {
@@ -64,4 +64,9 @@ $("#trigger").on("click", function() {
 
 $(".submit").on("click", function(ev) {
     ev.preventDefault();
+})
+
+
+$(".addUser").on("click", function() {
+    alert("Dodano użytkownika");
 })
