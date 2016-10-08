f4654010d5c96d024787da13d8496e879105f282
Jakub Swierczek <jakub.swierczek@outlook.com>
2016-10-08 15:07:01 +0200

    Add files

---
+++ Backend/ImNew/ImNew.Infrastructure/RoleService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImNew.Domain.Model;
+using ImNew.Domain.Repositories;
+
+namespace ImNew.Infrastructure
+{
+	public class RoleService
+	{
+		public IRepository<Role> Repository { get;  }
+
+		public RoleService(IRepository<Role> repository )
+		{
+			Repository = repository;
+		}
+
+		public IEnumerable<DtoRole> GetAllRoles()
+		{
+			return Repository.GetAll().Select(x => new DtoRole {Id = x.Id, Name = x.Name});
+		}
+
+		public void AddRole(string role)
+		{
+			Repository.Add(new Role
+			{
+				Name = role
+			});
+		}
+
+
+	}
+}
---
+++ Backend/ImNew/ImNew/Controllers/RoleController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ImNew.Domain.Repositories;
+using ImNew.Infrastructure;
+using ImNew.Models;
+
+namespace ImNew.Controllers
+{
+	[RoutePrefix("api/roles")]
+	public class RoleController : ApiController
+    {
+		public RoleService RoleService = new RoleService(new RoleRepository(Database.DbContext));
+
+		[Route("")]
+		public IHttpActionResult Get()
+		{
+			return Ok(RoleService.GetAllRoles());
+		}
+
+		[HttpPost]
+		[Route]
+		public IHttpActionResult Post([FromBody]string value)
+		{
+			RoleService.AddRole(value);
+			return Ok();
+		}
+
+		[Route("init")]
+		[HttpGet]
+		public IHttpActionResult InitData()
+	    {
+		    RoleService.AddRole("Senior Developer");
+			RoleService.AddRole("Scrum Master");
+			RoleService.AddRole("Product Owner");
+			RoleService.AddRole("Medium Developer");
+		    return Ok();
+	    }
+	}
+}
