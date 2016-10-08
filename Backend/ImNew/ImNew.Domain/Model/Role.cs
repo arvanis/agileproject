c09a37d5fa3dbe45d28d62ef4dfba5941fe504db
Jakub Swierczek <jakub.swierczek@outlook.com>
2016-10-08 12:36:29 +0200

    Added repositories

--- Backend/ImNew/ImNew.Domain/Model/Role.cs
+++ Backend/ImNew/ImNew.Domain/Model/Role.cs
@@ -1,7 +1,12 @@
-﻿namespace ImNew.Domain.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImNew.Domain.Model
 {
 	public class Role
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public string Name { get; set; }
 	}
---
+++ Backend/ImNew/ImNew.Domain/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImNew.Domain.Model;
+
+namespace ImNew.Domain.Repositories
+{
+	class UserRepository: IRepository<User>
+	{
+		public ImNewContext DbContext { get; }
+
+		public UserRepository()
+		{
+			DbContext = new ImNewContext();
+		}
+
+		public void Add(User item)
+		{
+			DbContext.Users.Add(item);
+			Save();
+
+		}
+
+		public void Edit(User item)
+		{
+			DbContext.Entry(item).State = EntityState.Modified;
+			Save();
+		}
+
+		public User GetSingle(int id)
+		{
+			return DbContext.Users.FirstOrDefault(x => x.Id == id);
+		}
+
+		private void Save()
+		{
+			DbContext.SaveChanges();
+		}
+	}
+}
