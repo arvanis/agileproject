f12bb79c45358be38a04935486706c531ab28687
Marki-Komputer <Marki-Komputer@MARKIPC>
2016-10-08 13:52:29 +0200

    Added surname, name, id, roleID, hobbies list

--- Backend/ImNew/ImNew.Infrastructure/DtoUserDetails.cs
+++ Backend/ImNew/ImNew.Infrastructure/DtoUserDetails.cs
@@ -8,9 +8,17 @@ namespace ImNew.Infrastructure
 {
     class DtoUserDetails
     {
-        public List<string> Technologies { get; set; }              
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int ID { set; get; }
+
+
+        public List<string> Technologies { get; set; }  
+                    
         public string Role { get; set; }
-        public string Hobbies { get; set; }
+        public int RoleID { get; set; }
+
+        public List<string> Hobbies { get; set; }
         public string About { get; set; }
     }
 }
